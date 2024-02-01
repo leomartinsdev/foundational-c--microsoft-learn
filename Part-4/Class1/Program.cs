092e1d0f03f926418d1cead8c8a68ae0454e59cf
Leonardo Martins <leonardogustavo@outlook.com>
2024-02-01 11:18:15 -0300

    Algorithms of the day

--- Part-4/Class1/Program.cs
+++ Part-4/Class1/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void division(int num1, int num2)
+{
+    int quociente = num1 / num2;
+    int resto = num1 % num2;
+
+    Console.WriteLine($"Quociente = {quociente}, Resto = {resto}");
+}
+
+division(2, 5);
